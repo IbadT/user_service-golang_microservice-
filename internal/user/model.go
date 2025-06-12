703622fb38f51eba1ef6abaaa67f08f1764b7f22
Eduard Kononovich <101503726+IbadT@users.noreply.github.com>
2025-06-12 16:49:41 +0300

    added task

--- internal/user/model.go
+++ internal/user/model.go
@@ -5,8 +5,16 @@ import (
 	"gorm.io/gorm"
 )
 
+type Task struct {
+	gorm.Model
+	ID     uuid.UUID `json:"id" gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+	Title  string    `json:"title" gorm:"type:varchar(255);not null"`
+	UserID uuid.UUID `json:"user_id" gorm:"type:uuid"`
+}
+
 type User struct {
 	gorm.Model
 	Id    uuid.UUID `json:"id" gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
 	Email string    `json:"email" gorm:"type:varchar(255);unique;not null"`
+	Tasks []Task    `gorm:"foreignKey:UserID"`
 }
